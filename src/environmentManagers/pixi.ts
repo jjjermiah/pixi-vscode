f720ca2c499764f04b320d96dffce573776cbb3e
Jermiah Joseph <jermiahjoseph98@gmail.com>
2024-11-15 10:21:00 -0500

    feat: Enhance Pixi class to improve Python interpreter path resolution and code formatting

--- src/environmentManagers/pixi.ts
+++ src/environmentManagers/pixi.ts
@@ -6,121 +6,136 @@
 // https://github.com/microsoft/vscode-python/pull/22968/files#diff-584fabe84516093f6d4c24d3a247862ebb66ded6a70382347e06c84047d0f269
 
 import {
-	PixiProjectInfo,
-	PixiInfo,
-	PixiTask,
-	environments_info,
+  PixiProjectInfo,
+  PixiInfo,
+  PixiTask,
+  environments_info,
 } from "../types";
 import { execShellWithTimeout } from "../common/shell";
 import { PixiPlatform, PixiCommand, PixiProjectType } from "../enums";
-import * as path from 'path';
+import * as path from "path";
+import * as fs from "fs/promises";
 
 export class Pixi {
-	// save the output of the pixi info command
-	public pixiInfo!: PixiInfo | undefined;
+  // save the output of the pixi info command
+  public pixiInfo!: PixiInfo | undefined;
 
-	constructor(private readonly command: string, private cwd: string) {
-		this.getPixiInfo()
-			.then((info) => {
-				this.pixiInfo = info;
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	}
+  constructor(private readonly command: string, private cwd: string) {
+    this.getPixiInfo()
+      .then((info) => {
+        this.pixiInfo = info;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
-	public async getPixiInfo(
-		manifestPath?: string
-	): Promise<PixiInfo | undefined> {
-		const info = await execShellWithTimeout(
-			`${this.command} info --json ${
-				manifestPath ? `--manifest-path ${manifestPath}` : ""
-			}`,
-			5000,
-			this.cwd
-		)
-			.then((output) => JSON.parse(output) as PixiInfo)
-			.catch((error) => {
-				console.error(error);
-				return undefined;
-			});
-		return info;
-	}
+  public async getPixiInfo(
+    manifestPath?: string
+  ): Promise<PixiInfo | undefined> {
+    const info = await execShellWithTimeout(
+      `${this.command} info --json ${
+        manifestPath ? `--manifest-path ${manifestPath}` : ""
+      }`,
+      5000,
+      this.cwd
+    )
+      .then((output) => JSON.parse(output) as PixiInfo)
+      .catch((error) => {
+        console.error(error);
+        return undefined;
+      });
+    return info;
+  }
 
-	/**
-	 * Get the user's default Pixi (aka rattler-build defined) platform
-	 *
-	 * @returns  the default platform
-	 */
-	public async PixiDefaultPlatform(): Promise<PixiPlatform | undefined> {
-		return this.getPixiInfo().then((info) => info?.platform);
-	}
+  /**
+   * Get the user's default Pixi (aka rattler-build defined) platform
+   *
+   * @returns  the default platform
+   */
+  public async PixiDefaultPlatform(): Promise<PixiPlatform | undefined> {
+    return this.getPixiInfo().then((info) => info?.platform);
+  }
 
-	public async Channels(
-		manifestPath?: string
-	): Promise<string[] | undefined> {
-		const allChannels: string[] = [];
-		await this.EnvironmentInfo(manifestPath).then((envs) => {
-			envs?.forEach((env) => {
-				allChannels.push(...env.channels);
-			});
-		});
-		// remove duplicates
-		return Array.from(new Set(allChannels));
-	}
+  public async Channels(manifestPath?: string): Promise<string[] | undefined> {
+    const allChannels: string[] = [];
+    await this.EnvironmentInfo(manifestPath).then((envs) => {
+      envs?.forEach((env) => {
+        allChannels.push(...env.channels);
+      });
+    });
+    // remove duplicates
+    return Array.from(new Set(allChannels));
+  }
 
-	public async EnvironmentInfo(
-		manifestPath?: string
-	): Promise<environments_info[] | undefined> {
-		return this.getPixiInfo(manifestPath ? manifestPath : undefined).then(
-			(info) => info?.environments_info
-		);
-	}
+  public async EnvironmentInfo(
+    manifestPath?: string
+  ): Promise<environments_info[] | undefined> {
+    return this.getPixiInfo(manifestPath ? manifestPath : undefined).then(
+      (info) => info?.environments_info
+    );
+  }
 
-	public async EnvironmentNames(
-		manifestPath?: string
-	): Promise<string[] | undefined> {
-		return this.EnvironmentInfo(manifestPath).then((info) =>
-			info?.map((env) => env.name)
-		);
-	}
+  public async EnvironmentNames(
+    manifestPath?: string
+  ): Promise<string[] | undefined> {
+    return this.EnvironmentInfo(manifestPath).then((info) =>
+      info?.map((env) => env.name)
+    );
+  }
 
-	public async EnvironmentPrefixes(
-		manifestPath?: string
-	): Promise<string[] | undefined> {
-		return this.EnvironmentInfo(manifestPath).then((info) =>
-			info?.map((env) => env.prefix)
-		);
-	}
+  public async EnvironmentPrefixes(
+    manifestPath?: string
+  ): Promise<string[] | undefined> {
+    return this.EnvironmentInfo(manifestPath).then((info) =>
+      info?.map((env) => env.prefix)
+    );
+  }
 
-	public async Features(
-		manifestPath?: string
-	): Promise<string[] | undefined> {
-		return this.EnvironmentInfo(manifestPath)
-			.then((info) => info?.flatMap((env) => env.features))
-			.then((features) => Array.from(new Set(features)));
-	}
+  public async Features(manifestPath?: string): Promise<string[] | undefined> {
+    return this.EnvironmentInfo(manifestPath)
+      .then((info) => info?.flatMap((env) => env.features))
+      .then((features) => Array.from(new Set(features)));
+  }
 
-	public async Tasks(manifestPath?: string): Promise<PixiTask[] | undefined> {
-		return this.EnvironmentInfo(manifestPath).then((info) =>
-			// for each task within an environment, create a PixiTask
-			info?.flatMap((env) =>
-				env.tasks.map((task) => ({
-					name: task,
-					environment: env.name,
-				}))
-			)
-		);
-	}
+  public async Tasks(manifestPath?: string): Promise<PixiTask[] | undefined> {
+    return this.EnvironmentInfo(manifestPath).then((info) =>
+      // for each task within an environment, create a PixiTask
+      info?.flatMap((env) =>
+        env.tasks.map((task) => ({
+          name: task,
+          environment: env.name,
+        }))
+      )
+    );
+  }
 
-	/**
-	 * Returns the path to the Python interpreter used by the workspace.
-	 *
-	 * @param env_info - The environments_info object containing the prefix path to the Python interpreter.
-	 * @returns The path to the Python interpreter.
-	 */
-	public async getPythonInterpreterPath(env_info: environments_info): Promise<string> {
-		const pythonExecutable = process.platform === 'win32' ? 'python.exe' : 'python';
-		return path.join(env_info.prefix, 'bin', pythonExecutable);
-	}
+  /**
+   * Returns the path to the Python interpreter used by the workspace.
+   *
+   * @param env_info - The environments_info object containing the prefix path to the Python interpreter.
+   * @returns The path to the Python interpreter.
+   */
+  public async getPythonInterpreterPath(
+    env_info: environments_info
+  ): Promise<string> {
+    const pythonExecutable =
+      process.platform === "win32" ? "python.exe" : "python";
+    const binPath = path.join(env_info.prefix, "bin", pythonExecutable);
+    const rootPath = path.join(env_info.prefix, pythonExecutable);
+
+    try {
+      await fs.access(binPath);
+      return binPath;
+    } catch {
+      try {
+        await fs.access(rootPath);
+        return rootPath;
+      } catch {
+        throw new Error(
+          `Python interpreter not found in ${binPath} or ${rootPath}`
+        );
+      }
+    }
+  }
 }
