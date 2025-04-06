e81353c3e9240fc95d6c7c4a478bb9162b290694
Jermiah Joseph <jermiahjoseph98@gmail.com>
2025-04-06 10:20:23 -0400

    refactor: update import paths and clean up extension activation logic

--- src/extension.ts
+++ src/extension.ts
@@ -4,29 +4,28 @@
 import * as vscode from "vscode";
 
 import * as log from "./common/logging";
-import { findPixiProjects } from "./common/filesystem";
-// import { EXTENSION_NAME } from "./common/constants";
+import { findPixiProjects } from "./common/project_finder";
+import { EXTENSION_NAME } from "./common/constants";
 // import { PixiTaskProvider } from "./taskProvider/pixiTaskProvider";
 // import { PixiWorkspaceInitializer } from "./initPixiWorkspace";
 
-// const Cache = require("vscode-cache");
-
+const Cache = require("vscode-cache");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-	// Create a new output channel for the extension
-	const outputChannel = vscode.window.createOutputChannel(EXTENSION_NAME, {
-		log: true,
-	});
+  // Create a new output channel for the extension
+  const outputChannel = vscode.window.createOutputChannel(EXTENSION_NAME, {
+    log: true,
+  });
 
-	// Create a new cache instance
-	const cache = new Cache(context);
+  // Create a new cache instance
+  const cache = new Cache(context);
 
-	// Register the output channel and logger
+  // Register the output channel and logger
   context.subscriptions.push(outputChannel, log.registerLogger(outputChannel));
 
-	// Find Pixi projects in the workspace
+  // Find Pixi projects in the workspace
   let pixi_projects = await findPixiProjects();
 
   // Tell user
@@ -41,20 +40,23 @@ export async function activate(context: vscode.ExtensionContext) {
     );
   }
 
-  // Register the Pixi Task Provider
-  context.subscriptions.push(
-    vscode.tasks.registerTaskProvider(
-      "Pixi",
-      new PixiTaskProvider(pixi_projects)
-    )
-  );
+  // // Register the Pixi Task Provider
+  // context.subscriptions.push(
+  //   vscode.tasks.registerTaskProvider(
+  //     "Pixi",
+  //     new PixiTaskProvider(pixi_projects)
+  //   )
+  // );
 
-	const workspaceInitializer = new PixiWorkspaceInitializer(cache, pixi_projects);
-	// Register the Pixi Init command 
+  const workspaceInitializer = new PixiWorkspaceInitializer(
+    cache,
+    pixi_projects
+  );
+  // Register the Pixi Init command
   context.subscriptions.push(
     vscode.commands.registerCommand("pixi-vscode.init", async () => {
-			await workspaceInitializer.initPixiWorkspace();
-		})
+      await workspaceInitializer.initPixiWorkspace();
+    })
   );
 }
 
